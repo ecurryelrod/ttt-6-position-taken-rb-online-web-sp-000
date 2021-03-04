4838b6a9dc5f15e8be90efe22b0875e51516f22a
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-03-04 19:28:29 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@
 def position_taken?(board, index)
   if board[index] == "X" || board[index] == "O"
     true
-  elsif board[index] == " " || ""
+  elsif board[index] == " " || board[index] == "" || board[index] == nil
     false
   end
 end
